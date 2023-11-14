cbde4b2febf297b373b98130771a2d285d34c035
AngeloBla <angeloblajic@gmail.com>
2023-11-14 09:59:13 +0100

    Komplettlöschung
    !

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Einloggen from "./components/kunden"; 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.render(<Einloggen />, document.getElementById('root'));
-
