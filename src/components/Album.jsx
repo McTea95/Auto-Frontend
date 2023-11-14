70736839f9941e37cbf709ed13e02d7dad32cf2d
AngeloBla <angeloblajic@gmail.com>
2023-11-14 09:16:39 +0100

    update album
    edit button entfernt

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -189,7 +189,6 @@ class Album extends Component {
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
                     <small className="text-body-secondary">9 mins</small>
                   </div>
