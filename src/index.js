f3b25705dcdd0bbc8f845a83a6abf6b0e6310a20
Martin Krueger <122008999+Martin20231@users.noreply.github.com>
2023-11-14 10:18:51 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDom from "react-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
-import App from "./components/App"
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-ReactDom.render(<App></App>,document.getElementById('root'));
